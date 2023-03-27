893d812a9da6f4d24a1b17a9bdca3fe4e8b7a888
uquo <uquo@fresnou.org>
2023-03-27 09:36:50 -0700

    initial commit

--- arrayNotes2.js
+++ arrayNotes2.js
@@ -1,9 +1,85 @@
-function printSecond(arr){
-    console.log( arr[1] )
-}
-
-printSecond( ['a', 'b', 'c', 'd'] )
-printSecond( [7, 5, 3, 1] )
-
 //1
-console.log(printSecond[0])
+// function printFirst(arr){
+//     console.log( arr[0] )
+// }
+// printFirst( [7, 5, 3, 1] )
+
+//2
+// function printLast(arr){
+//     console.log( arr[3] )
+// }
+// printLast( [7, 5, 3, 1] )
+
+//3
+function emptyArray(arr){
+    if (arr.length ==0){
+        console.log('Empty array')
+    }else{
+        console.log( arr[0] )
+    }
+
+}
+emptyArray( [] )
+
+function emptyArray2(arr){
+    if (arr.length ==0){
+        console.log('Empty array')
+    }else{
+        console.log( arr[3] )
+    }
+
+}
+emptyArray2( [] )
+
+//4
+
+
+//5
+// function thirdElement(arr){
+//     console.log( arr[2] )
+//     console.log( arr[5] )
+//     console.log( arr[8] )
+// }
+// thirdElement([3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+//6
+// function secondElement(arr){
+//     console.log( arr[1] )
+//     console.log( arr[3] )
+//     console.log( arr[5] )
+//     console.log( arr[7] )
+//     console.log( arr[9] )
+// }
+// secondElement([3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+//7
+// function greaterThan(arr){
+//     console.log( arr[1] )
+//     console.log( arr[2] )
+//     console.log( arr[3] )
+//     console.log( arr[4] )
+//     console.log( arr[5] )
+//     console.log( arr[6] )
+//     console.log( arr[7] )
+//     console.log( arr[8] )
+//     console.log( arr[9] )
+// }
+// greaterThan([3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+//8
+// function uniqueValue(arr){
+//     console.log(arr[4])
+//     console.log(arr[14])
+//     console.log(arr[18])
+// }
+// uniqueValue([3,3,3,3,4,3,3,5,5,5,5,5,5,5,8,5,5,5,9,5,5])
+
+//9
+
+
+//10
+// function notUnique(arr){
+//     console.log(arr[0])
+//     console.log(arr[7])
+// }
+// notUnique([3,3,3,3,4,3,3,5,5,5,5,5,5,5,8,5,5,5,9,5,5])
