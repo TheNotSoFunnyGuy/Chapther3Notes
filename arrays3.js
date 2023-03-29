30b90b1422e41241d00e6477bdf755b9c3da2e02
uquo <uquo@fresnou.org>
2023-03-29 10:29:24 -0700

    initial commit

---
+++ arrays3.js
@@ -0,0 +1,49 @@
+const readLine = require('readline-sync')
+
+//1
+// function countUp(n){
+//     let newArray=[];
+//     for(let i=1; i<=n; i++){
+//         newArray.push(i);
+//     }
+//     console.log(newArray);
+// }
+//
+// countUp(5);
+
+//2
+// function countUp(n){
+//     if (n<=0){
+//         console.log('Invalid');
+//     }else{
+//     let newArray=[];
+//     for(let i=1; i<=n; i++){
+//         newArray.push(i);
+//     }
+//     console.log(newArray);
+// }}
+//
+// countUp(-1);
+
+//3
+
+
+//4
+
+
+//5
+
+
+//6
+
+
+//7
+
+
+//8
+
+
+//9
+
+
+//10
