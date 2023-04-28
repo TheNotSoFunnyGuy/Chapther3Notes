0adeee9372aa4526004cda427db447a8f873277f
uquo <uquo@fresnou.org>
2023-04-28 10:33:36 -0700

    initial commit

--- comboMenu.js
+++ comboMenu.js
@@ -17,60 +17,83 @@ const beverageOptions = {
     medium: 1.75,
     large: 2.25
 };
-let beveragePrice;
+
 let beverageChoice = readline.question("Would you like a beverage? (yes/no): ");
 let beverage = 0;
+let beverageSize = "no"
+let beveragePrice = 0
 if (beverageChoice.toLowerCase() === "yes") {
-    let beverageSize = readline.question("What size beverage would you like? (small, medium, or large): ")
+    beverageSize = readline.question("What size beverage would you like? (small, medium, or large): ");
     beveragePrice = beverageOptions[beverageSize.toLowerCase()];
+    console.log(`You selected a ${beverageSize} beverage for ${beveragePrice.toFixed(2)}`);
+}
+else {
+    beverageSize = "no"
+    beveragePrice = 0
 }
 
-const friesOptions = {
-    small: 1.00,
-    medium: 1.50,
-    large: 2.00
-};
+let total1 = sandwichPrice + beveragePrice;
 
-let friesChoice = readline.question("would you like fries? (yes/no): ");
-
-let friesPrice = 0;
-if (friesChoice.toLowerCase() === "yes"){
-    let friesSize = readline.question("What size fries would you like? (small, medium, or large): ");
-    friesPrice = friesOptions[friesSize.toLowerCase()];
-
-    if (friesSize.toLowerCase() === "small"){
-        let megaFries = readline.question("Would you like to mega-size your fries? (yes/no): ");
-        if (megaFries.toLowerCase() === "yes"){
-            friesPrice = friesOptions["large"];
-            
-        }
-    } 
-}
-
-const ketchupPackPrice =0.25;
-
-let ketchupPackets = readline.question("How many ketchup packets would you like?");
-let ketchupPrice = ketchupPackets * ketchupPackPrice;
-
-const taxRate = 0.07;
-const ComboDiscount = 1.00;
-
-
-let subtotal = sandwichPrice + beveragePrice + friesPrice + ketchupPrice;
-let comboDiscount = 0;
-if (sandwichPrice > 0 && beveragePrice > 0 && friesPrice > 0){
-    subtotal -= ComboDiscount;
-    comboDiscount = ComboDiscount;
-}
-
-let tax = subtotal * taxRate;
-let total = subtotal + tax;
-
-console.log("------------------------");
 console.log(`${sandwichChoice} sandwich: ${sandwichPrice.toFixed(2)}`);
-console.log(`${beverageSize} beverage: ${beveragePrice.toFixed(2)}`);
-console.log(`${friesSize} fries: ${friesPrice.toFixed(2)}`);
-console.log(`Ketchup packets: ${ketchupPrice.toFixed(2)}`);
-console.log("------------------------");
-console.log(`Combo discount: ${comboDiscount.toFixed(2)}`)
-console.log(`Subtotal: ${total.toFixed(2)}`);
+if (beverageChoice.toLowerCase() === "yes") {
+    console.log(`${beverageSize} beverage: ${beveragePrice.toFixed(2)}`);
+}else{
+    console.log("No beverage")
+}
+console.log(`Your total is ${total1} dollars`)
+
+
+
+
+// const friesOptions = {
+//     small: 1.00,
+//     medium: 1.50,
+//     large: 2.00
+// };
+//
+// let friesChoice = readline.question("would you like fries? (yes/no): ");
+//
+// let friesPrice = 0;
+// if (friesChoice.toLowerCase() === "yes"){
+//     let friesSize = readline.question("What size fries would you like? (small, medium, or large): ");
+//     friesPrice = friesOptions[friesSize.toLowerCase()];
+// }
+//
+//     if (friesSize.toLowerCase() === "small"){
+//         let megaFries = readline.question("Would you like to mega-size your fries? (yes/no): ");
+//         if (megaFries.toLowerCase() === "yes"){
+//             friesPrice = friesOptions["large"];
+//
+//         }
+//     }
+//
+//
+// const ketchupPackPrice =0.25;
+//
+// let ketchupPackets = readline.question("How many ketchup packets would you like?");
+// let ketchupPrice = ketchupPackets * ketchupPackPrice;
+//
+// const taxRate = 0.07;
+// const ComboDiscount = 1.00;
+//
+//
+// let subtotal = sandwichPrice + beveragePrice + friesPrice + ketchupPrice;
+// let comboDiscount = 0;
+// if (sandwichPrice > 0 && beveragePrice > 0 && friesPrice > 0){
+//     subtotal -= ComboDiscount;
+//     comboDiscount = ComboDiscount;
+// }
+//
+// let tax = subtotal * taxRate;
+// let total = subtotal + tax;
+//
+// console.log("------------------------");
+// console.log(`${sandwichChoice} sandwich: ${sandwichPrice.toFixed(2)}`);
+// console.log(`${beverageSize} beverage: ${beveragePrice.toFixed(2)}`);
+// console.log(`${friesSize} fries: ${friesPrice.toFixed(2)}`);
+// console.log(`${ketchupPackets} Ketchup packets: ${ketchupPrice.toFixed(2)}`);
+// console.log("------------------------");
+// // console.log(`Combo discount: ${comboDiscount.toFixed(2)}`);
+// console.log(`${tax.toFixed(2)}`);
+// console.log(`Subtotal: ${total.toFixed(2)}`);
+// console.log("------------------------");
